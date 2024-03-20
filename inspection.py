534c7dd3740852df773ca3669fe8293fff845cdc
Haleigh <schwartzh2017@gmail.com>
2024-03-20 15:47:56 -0700

    cleaning

--- inspection.py
+++ inspection.py
@@ -1,3 +1,5 @@
+# Christy's work:
+
 import pandas as pd
 import numpy as np
 
@@ -11,3 +13,39 @@ pd.set_option('display.max_rows', None)
 print(df.isnull().any())
 
 print(df.loc[2])
+
+#--------------------------
+
+# Haleigh's edits/cleaning:
+
+# remove NAs
+df2 = df.dropna()
+df2.shape #only have 1048 rows left - should we use this one?
+df.shape #compared to 8378 rows when there are NAs
+
+#will continue with df2 but can just copy/paste with df if we decide to use that one instead
+
+#I think it's okay to keep some of the numeric data binned since that might help to find patterns between variables later, but I can be convinced otherwise. What do you all think?
+
+#remove single quotes from race, race_o cols, and field cols
+cols_to_check = ['race', 'race_o', 'field']
+df2[cols_to_check] = df2[cols_to_check].replace({'\'':''}, regex=True)
+df2['race'] #check
+df2['race_o'] #check
+df2['field'] #check
+
+#lowercase everything (including field since they weren't all upper or lowercase in field)
+df2['field'] = df2['field'].apply(lambda x: x.lower()) #I couldn't get this to work using cols_to_check for some reason. also tried str.lower but it didn't work, so did it individually instead since only three cols
+df2['race'] = df2['race'].apply(lambda x: x.lower())
+df2['race_o'] = df2['race_o'].apply(lambda x: x.lower())
+df2['race'] #check
+df2['race_o'] #check
+df2['field'] #check
+
+#check that all ? were removed/detected as NA -- if use df instead, make sure to convert all ? to NA 
+"?" in df2 #false, so they were removed 
+
+# export to csv
+df2.to_csv('clean_speeddating.csv')
+
+#--------------------------
